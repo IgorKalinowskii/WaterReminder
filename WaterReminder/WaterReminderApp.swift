40ca5ae6d981dfd878f60ac39a0c209ce6fe178f
IgorKalinowskii <s15046@pjwstk.edu.pl>
2024-04-03 15:02:08 +0200

    Initial Commit

---
+++ WaterReminder/WaterReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterReminderApp.swift
+//  WaterReminder
+//
+//  Created by Wiktoria Jasińska on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WaterReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
